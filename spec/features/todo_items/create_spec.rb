9dfc84b489a31bd9f4169bd13abe812589427f66
Aaron Williamson <wikea1@gmail.com>
2014-08-21 22:47:07 -0700

    Add specs and code for editing todo items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Adding todo items" do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") } 
-	
+
 	def visit_todo_list(list)
 		visit "/todo_lists"
 		within "#todo_list_#{list.id}" do
