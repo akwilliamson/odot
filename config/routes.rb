266951d18e49b41f0b5628db656e194e41963d4e
Aaron Williamson <wikea1@gmail.com>
2014-08-21 18:52:40 -0700

    Write tests for todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'todo_lists#index'
+  
   resources :todo_lists do
     resources :todo_items
   end
 
   resources :todo_items
-
-
-  root 'todo_lists#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
